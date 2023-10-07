c7214fe41f380169da791e0153547dc30b8cd1d3
Salar Gholami <salargholami021@gmail.com>
2023-10-07 17:08:01 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { default: axios } = require("axios");
+
+require("dotenv").config();
+
+console.log(process.env.SECRET_KEY);
+
+// console.log(`connecting to port 5000`);
+
+// axios.get("https://api.koft.ir");
+
+//1.clean code
+//2. security
